67575c45533c6183ce1354e2856a3196c0e0ef35
Bendi11 <bkliebmann@gmail.com>
2022-05-07 10:20:26 -0700

    switched to gtk

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,11 +1,5 @@
 #include "window.hpp"
 #include <lib.hpp>
-#include <FL/Fl.H>
 
 int main(int argc, char * argv[]) {
-    MainWindow win{};
-    
-    win.end();
-    win.show(argc, argv);
-    return Fl::run();
 }
